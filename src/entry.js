6e7bf25820c38a8888c032458a5451a4274b255d
Acaruso <anthonycaruso102@gmail.com>
2020-09-11 18:18:05 -0400

    stuff

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+// ball stuff //////////////////////////////////////////////////
+
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
@@ -17,9 +19,51 @@ function drawBall() {
   ctx.closePath();
 }
 
+// paddle stuff ////////////////////////////////////////////////
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if(e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = true;
+  }
+  else if(e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = false;
+  }
+  else if(e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = false;
+  }
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// main ////////////////////////////////////////////////////////
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBall();
+  drawPaddle();
+
   x += dx;
   y += dy;
 
