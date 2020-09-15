59f87a5f8d33bda6ed8514bca0a2928011c835be
Acaruso <anthonycaruso102@gmail.com>
2020-09-15 17:35:51 -0400

    dialog stuff

--- src/modules/dialog.js
+++ src/modules/dialog.js
@@ -1,20 +1,30 @@
-function drawDialog(text, canvas) {
+function drawDialog(text, pixels, x, y, canvas) {
   let ctx = canvas.getContext('2d');
-  ctx.font = '48px serif';
-  const x = canvas.width / 3;
-  const y = canvas.height / 2;
+  ctx.font = `${pixels}px sans-serif`;
+  // const x = canvas.width / 3;
+  // const y = canvas.height / 2;
   ctx.fillText(text, x, y);
 }
 
 function drawWinDialog(status, canvas) {
   if (status === "win") {
-    drawDialog("You win", canvas);
+    const pixels = 32;
+    const x = canvas.width / 3;
+    const y = canvas.height / 2;
+    drawDialog("You win", pixels, x, y, canvas);
+    const newY = y + pixels;
+    drawDialog("press enter", pixels, x, newY, canvas);
   }
 }
 
 function drawLostDialog(status, canvas) {
   if (status === "lost") {
-    drawDialog("You lost", canvas);
+    const pixels = 32;
+    const x = canvas.width / 3;
+    const y = canvas.height / 2;
+    drawDialog("You lost", pixels, x, y, canvas);
+    const newY = y + pixels;
+    drawDialog("press enter", pixels, x, newY, canvas);
   }
 }
 
